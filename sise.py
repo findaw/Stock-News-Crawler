d91b37030bdd86248e7e6c110ac7f1faf6801560
findaw <wqre1@naver.com>
2021-06-07 08:06:05 +0900

    Create sise-calc

---
+++ sise.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+# : sise 정보 데이터 프레임 전처리
+sise_data = pd.read_csv('data/sise_day_digit.csv')
+sise_data = sise_data.drop(sise_data.columns[0], axis=1)
+sise_data['date'] = pd.to_datetime(sise_data['date'])
+sise_data.head()
+
+
+####
+# : 급상승한 종목과 날짜 추출
+# 0 code / 1 date / 2 close / 3 diff /4 open / 5 high / 6 low / 7 volume
+result = []
+rate_best = 4      # 하루 4%
+for code in set(sise_data.code):
+    code_sise_data = sise_data[sise_data.code==code].values
+    code_sise_data = code_sise_data[::-1]
+    open_sise = 0
+    for i in range(len(code_sise_data)-2, 0, -1):   # 전날 종가대비 오늘 종가 증가율(-4% or 4%)
+        close_pre = code_sise_data[i-1][2]          # 전날 종가
+        diff_day = code_sise_data[i][3]             # 증가 금액
+        rate = close_pre / diff_day                 # 증가율
+        if rate >= 4:
+            # code date rate 전날종가 오늘종가 volume
+            result.append([code, code_sise_data[i][1], rate, code_sise_data[i-1][2], code_sise_data[i][2], code_sise_data[i][7]])
+
+        
+result = pd.DataFrame(result)        
+result.to_csv('data/급등주포착.csv')
+    
+
+
+
+
+
+
+
+
