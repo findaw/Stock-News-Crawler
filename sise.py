5eb77307cc85b9b026e00aae4d91bf97d4a33940
findaw <qkrdpwls1836@gmail.com>
2021-06-07 10:12:33 +0900

    일별 급등/급락 계산하기

--- sise.py
+++ sise.py
@@ -11,23 +11,27 @@ sise_data.head()
 # : 급상승한 종목과 날짜 추출
 # 0 code / 1 date / 2 close / 3 diff /4 open / 5 high / 6 low / 7 volume
 result = []
-rate_best = 4      # 하루 4%
+rate_best = 0.04      # 하루 4%
 for code in set(sise_data.code):
     code_sise_data = sise_data[sise_data.code==code].values
     code_sise_data = code_sise_data[::-1]
     open_sise = 0
+    print(len(code_sise_data))
+    print(type(code_sise_data))
     for i in range(len(code_sise_data)-2, 0, -1):   # 전날 종가대비 오늘 종가 증가율(-4% or 4%)
         close_pre = code_sise_data[i-1][2]          # 전날 종가
         diff_day = code_sise_data[i][3]             # 증가 금액
-        rate = close_pre / diff_day                 # 증가율
-        if rate >= 4:
-            # code date rate 전날종가 오늘종가 volume
-            result.append([code, code_sise_data[i][1], rate, code_sise_data[i-1][2], code_sise_data[i][2], code_sise_data[i][7]])
+        rate = diff_day /close_pre                  # 증가율
+        if rate >= rate_best:
+            # code date rate 전날종가 오늘종가 diff volume
+            diff = code_sise_data[i][2] - code_sise_data[i-1][2]
+            result.append([code, code_sise_data[i][1], rate, code_sise_data[i-1][2], code_sise_data[i][2], diff, code_sise_data[i][7]])
 
         
-result = pd.DataFrame(result)        
+result = pd.DataFrame(result, columns=['code','date','rate', 'pre_close', 'close', 'diff', 'volume'])        
 result.to_csv('data/급등주포착.csv')
-    
+
+# 전체평균 약 10일에 1번씩 +-4%의 등락율을 보인다
 
 
 
