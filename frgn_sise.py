4f476dffddd9b59c2224fa10a24e90028b7f8186
findaw <qkrdpwls1836@gmail.com>
2021-06-20 19:24:21 +0900

    modify newslist&frgn_sise

--- frgn_sise.py
+++ frgn_sise.py
@@ -4,29 +4,39 @@ import requests
 from itertools import count
 import time
 result=[]
-item_code = pd.read_csv('data/sise_code_Top50.csv', dtype={'code':str})['code']
+item_code = pd.read_csv('data/sise_code_Top50.csv', dtype={'code':str})['code'][:10]
+print(item_code)
 for code in item_code:
     stop_loop = False    # stop_loop 정의
+    last_page_date = ""
     for page in count(1):
-        url = f'https://finance.naver.com/item/frgn.nhn?code=005930&page={page}'
+        url = f'https://finance.naver.com/item/frgn.nhn?code={code}&page={page}'
         html = requests.get(url, headers = {'User-Agent':'Mozilla/5.0'})
-        soup_data = BeautifulSoup(html.content,'html.parser')   
-        print(page)
-        print(code)
-        table = soup_data.findAll('table',{'class':'type2'})[1]  # table 원하는 것 찾기.  
+        soup_data = BeautifulSoup(html.content,'html.parser') 
+        table = soup_data.findAll('table',{'class':'type2'})[1]  # table 원하는 것 찾기. 
+
         for item in table.select('tr:not(.title1)'):
             item = item.select('td span')  # item = span list 
             if item == []:                # block은 colon 
                 continue 
             else:
-                result.append([num.text.strip() for num in item])
-                # 2021.06.18부터 2011.01.01 찾아보기
+                page_date = item[0].text
+                if last_page_date > page_date:
+                    last_page_date = page_date
+                else:
+                    stop_loop = True
+                    break
+
                 print(item[0].text)
-                if item[0].text == '2021.06.15':    
+                # 2021.06.18부터 2011.01.01 찾아보기
+                result.append([num.text.strip() for num in item])
+
+                if item[0].text == '2011.01.03':    
                     stop_loop = True    
                     break
+                
         if (stop_loop):            # 이중반복문 out 
             break
 result = pd.DataFrame(result, columns = ['date', 'close', 'diff', 'diff_rate',
                         'volume', ' int_dff', 'frgn_dff', 'frgn_share', 'frgn_rate'])
-result.to_csv('data/frgn_invest_Top50.csv')
+result.to_csv('data/frgn_invest_Top10.csv')
--- newlist50.py
+++ newlist50.py
@@ -8,14 +8,12 @@ from multiprocessing import Process
 top_no=5
 kospi_top = pd.read_csv('data/sise_market_sum.csv', index_col=[0])[:top_no]
 
-def worker(data, start_index):
-    start_date = dt.strptime('20110119', '%Y%M%d')  
-    end_date = dt.strptime('20210619', '%Y%M%d')
+def worker(data, start_index, start, end):
+    start_date = dt.strptime(start, '%Y%M%d')  
+    end_date = dt.strptime(end, '%Y%M%d')
     for index in range(start_index,start_index+1):
         try:
             search_word, code = data.title[index], data.code[index]
-            type(search_word)
-            print(search_word)
         except Exception as e:
             print('[Error] : ', e)
             continue
@@ -30,7 +28,7 @@ def worker(data, start_index):
         # end : for in date:
         try:
             df_news = pd.DataFrame(result[1:], columns=['code', 'date', 'title', 'link', 'content'])
-            df_news.to_csv('data/news/%s_네이버_뉴스목록_10년.csv'%(search_word))
+            df_news.to_csv('data/news/%s_네이버_뉴스목록_1년_%s~%s.csv'%(search_word, start, end))
         except:
             pass
     # end : for in code:
@@ -38,13 +36,16 @@ def worker(data, start_index):
 
 if __name__ == '__main__':
     procs = []
-    for n in range(3,top_no):
+    for n in range(1, 2):
         index = n
         data = kospi_top[index:index+1]
-        # worker(data, index)
-        proc = Process(target =worker, args=(data, index))
-        procs.append(proc)
-        proc.start()
+        start_date = [date for date in range(20100101,20210620,10000)]
+        end_date = [date for date in range(20110101,20220620,10000)]
+        for start, end in zip(start_date, end_date):
+            worker(data, index, str(start),str(end))
+            # proc = Process(target =worker, args=(data, index, start, end))
+            # procs.append(proc)
+            # proc.start()
 
-    for proc in procs:
-        proc.join()
+    # for proc in procs:
+    #     proc.join()
